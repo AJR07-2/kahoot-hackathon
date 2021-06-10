23d5bcce5353fd862a55a6ed865d020df31c4b8d
Ang Jun Ray <angjunray@live.com>
2021-06-10 11:52:00 +0800

    stuff

--- Kahoot Hackathon/Kahoot Hackathon/Kahoot_HackathonApp.swift
+++ Kahoot Hackathon/Kahoot Hackathon/Kahoot_HackathonApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Kahoot_HackathonApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
